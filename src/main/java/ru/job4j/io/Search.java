79d505cd74e9dbb9d28e32c970cb2f3d1f2699cb
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-03-17 18:41:48 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,11 +7,26 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * 5. Валидация параметров запуска.
+ * Валидация - это процесс проверки данных перед выполнением операции.
+ * Блок if - это валидация аргументов. Если вы запустите эту программу без указания аргументов, то она упадет с исключением.
+ * Откройте пункт Edit configuration. Добавьте путь в пункт Program agruments. Сохраните и запустите программу.
+ * Если пользователь забыл указать параметры, то в консоли он увидит в чем причина и как ее поправить.
+ *
+ * Задача:
+ * Программа должна запускаться с параметрами. Первый параметр - начальная папка. Второй параметр - расширение файлов,
+ * которые нужно искать. Необходимо добавить валидацию данных параметров.
+ */
+
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
