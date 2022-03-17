bbb501d0cf79b2f0e009eb511c2b39bc183a5842
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-03-17 17:25:47 +0300

    4.2. Поиск дубликатов [#315066 #275597] v.02

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,8 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 
 /**
  * 4.2. Поиск дубликатов
@@ -30,21 +31,22 @@ import java.util.HashSet;
  */
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    HashSet<Path> dublicates = new HashSet<Path>();
-    HashMap<FileProperty, Path> collect = new HashMap<>();
+    HashMap<FileProperty, List<Path>> collect = new HashMap<>();
+
+    public void printResult () {
+        for (FileProperty key : collect.keySet())
+        if (collect.get(key).size() > 1) {
+            collect.get(key).forEach(System.out::println);
+        }
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty value = new FileProperty(Files.size(file), file.getFileName().toString());
         if (!collect.containsKey(value)) {
-            collect.put(value, file);
-        } else {
-            if (!dublicates.add(collect.get(value))) {
-                System.out.println(collect.get(value).toAbsolutePath());
-            }
-            dublicates.add(file);
-            System.out.println(file.toAbsolutePath());
+            collect.put(value, new ArrayList<Path>());
         }
+        collect.get(value).add(file);
         return super.visitFile(file, attrs);
     }
 }
