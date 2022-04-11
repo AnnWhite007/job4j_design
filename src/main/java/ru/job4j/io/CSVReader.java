ab11e05bbb919d9cd1ba5c0f02e5b3e1f6e2554c
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-04-11 16:32:20 +0300

    7. Scanner [#504791 #285940] v.02

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 7. Scanner
@@ -19,7 +20,7 @@ import java.util.*;
  * С помощью метода userDelimiter() указывается нужный разделитель.
  * Метод useRadix() указывает в какой системе счисления находятся входные числа.
  * Если Scanner работает с внешними источниками его нужно использовать с try-with-resources.
- *
+ * <p>
  * Задание:
  * Создать класс CSVReader. Задача класса прочитать данные из CSV файла и вывести их.
  * В качестве входных данных задается путь к файлу path, разделитель delimiter, приемник данных out и фильтр по столбцам filter.
@@ -28,41 +29,55 @@ import java.util.*;
  */
 
 public class CSVReader {
-    public static void handle(ArgsName argsName) throws Exception {
+    public static void handle(ArgsName argsName) {
         File in = new File(argsName.get("path"));
         if (!Files.exists(Paths.get(String.valueOf(in)))) {
             throw new IllegalArgumentException(String.format("Not exist %s", in.getAbsoluteFile()));
         }
         List<String> filter = Arrays.asList(argsName.get("filter").split(","));
-        List<String> rsl = new ArrayList<>();
 
         try {
             var scanner = new Scanner(in);
             String firstLine = scanner.nextLine();
             List<String> cutFirstLine = Arrays.asList(firstLine.split(argsName.get("delimiter")));
             StringBuffer sb = new StringBuffer();
+            sb.append(filter.stream()
+                    .map(n -> String.valueOf(n))
+                    .collect(Collectors.joining(";", "", "")) + System.lineSeparator());
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 List<String> values = Arrays.asList(line.split(argsName.get("delimiter")));
                 for (int i = 0; i < cutFirstLine.size(); i++) {
                     if (filter.contains(cutFirstLine.get(i))) {
-                        sb.append(values.get(i) + "; ");
+                        sb.append(values.get(i));
+                        if (i < cutFirstLine.size() - 3) {
+                            sb.append(";");
+                        }
                     }
                 }
                 sb.append(System.lineSeparator());
 
             }
-            if ("stdout".equals(argsName.get("out"))) {
+            if (!"stdout".equals(argsName.get("out"))) {
                 try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(argsName.get("out"))))) {
-                    out.print(out);
+                    out.print(sb);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             } else {
                 System.out.println(sb);
             }
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Usage java -jar dir.jar ROOT_FOLDER DELIMITER RECORDING_FOLDER FILTER");
+        }
+        ArgsName argsmap = ArgsName.of(args);
+        handle(argsmap);
+    }
+
 }
