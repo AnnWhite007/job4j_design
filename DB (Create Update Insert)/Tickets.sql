91ca502763e1d8542d9b59af5ef2ba694d4671e1
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-06-02 18:15:35 +0300

    1. Inner join [#373617]

---
+++ DB (Create Update Insert)/Tickets.sql
@@ -0,0 +1,34 @@
+create table ticket(
+    id serial primary key,
+    seat int,
+    date date
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255),
+    ticket_id int references ticket(id) unique
+);
+
+insert into ticket(seat, date) values (123, '2022-05-06');
+insert into ticket(seat, date) values (456, '2022-06-05');
+insert into ticket(seat, date) values (589, '2022-09-05');
+
+insert into people(name, ticket_id) values ('Ivan', 1);
+insert into people(name, ticket_id) values ('Anna', 2);
+insert into people(name, ticket_id) values ('Kate', 3);
+insert into people(name) values ('Kate');
+
+select * from people
+join ticket t
+on people.ticket_id = t.id;
+
+select pp.name, t.seat, t.date
+from people as pp
+join ticket as t
+on pp.ticket_id = t.id;
+
+select pp.name as Имя, t.seat as Место,
+t.date as Дата from people as pp
+join ticket as t
+on pp.ticket_id = t.id;
