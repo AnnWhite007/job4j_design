0bfbf7d9c0f6d48a1ee81e784364eb3468097ac7
AnnWhite007 <anuta_myrzilka@inbox.ru>
2022-06-22 13:11:27 +0300

    0.1. Statement [#379306] v.02

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,7 +1,6 @@
 package ru.job4j.jdbc;
 
-
-import java.io.FileReader;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
 import java.util.StringJoiner;
@@ -11,20 +10,21 @@ import java.util.StringJoiner;
  * Для исполнения операций существуют специальные интерфейсы: Statement, PrepareStatement.
  * Для выполнения запроса для объекта существуют 3 метода: execute(), executeUpdate(), executeQuery().
  * При их вызове мы должны передать в качестве аргумента SQL - запрос.
- *
+ * <p>
  * executeUpdate() используется как для выполнения операторов управления данными (DML - операторы),
  * например INSERT, UPDATE или DELETE, так и для операторов определения структуры базы данных (DDL - операторы),
  * например CREATE TABLE, DROP TABLE. Возвращает int – количество affected строк, т.е. количество строк на которые оказал влияние запрос.
  * Для операторов, которые не манипулируют строками, таких как CREATE TABLE или DROP TABLE, возвращаемое значение executeUpdate всегда равно нулю.
- *
+ * <p>
  * executeQuery() используется для выполнения операции SELECT и возвращает объект ResultSet, который позволяет пройтись по результатам запроса.
- *
+ * <p>
  * execute() используется для выполнения любых команд. Возвращает true, если результатом выполнения является ResultSet (то есть был выполнен SELECT запрос),
  * или false, если результатом является int (количество изменённых строк). Получить ResultSet
  * или количество строк мы можем с помощью последующего вызова getUpdateCount() или getResultSet().
- *
+ * <p>
  * Задание:
  * 1. Дан каркас класса. Реализовать его методы. Чтение настроек должно идти из файла *.properties
+ * https://www.postgresqltutorial.com/
  */
 
 public class TableEditor implements AutoCloseable {
@@ -33,57 +33,68 @@ public class TableEditor implements AutoCloseable {
 
     private Properties properties;
 
-    public TableEditor(Properties properties) throws Exception {
+    public TableEditor(Properties properties) {
         this.properties = properties;
-        initConnection();
+        try {
+            initConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initConnection() throws Exception {
-            Class.forName(properties.getProperty("driver"));
-            String url = properties.getProperty("url");
-            String login = properties.getProperty("login");
-            String password = properties.getProperty("password");
-            connection = DriverManager.getConnection(url,login,password);
+        Class.forName(properties.getProperty("driver"));
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
     }
 
-    /** Создает пустую таблицу без столбцов с указанным именем */
+    public void initAction(String command) throws Exception {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(command);
+            System.out.println(getTableScheme(connection, "newtable"));
+        }
+    }
+
+    /**
+     * Создает пустую таблицу без столбцов с указанным именем
+     */
     public void createTable(String tableName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE " + tableName);
-            System.out.println(getTableScheme(connection, "newtable"));
-        }
+        initAction("CREATE TABLE " + tableName + " ();");
     }
 
-    /** Удаляет таблицу по указанному имени */
+    /**
+     * Удаляет таблицу по указанному имени
+     */
     public void dropTable(String tableName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("DROP TABLE " + tableName);
-            System.out.println(getTableScheme(connection, "newtable"));
-        }
-    }
-    /** Добавляет столбец в таблицу */
-    public void addColumn(String tableName, String columnName, String type) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("ALTER TABLE '" + tableName + "' ADD '" + columnName + " " + type);
-            System.out.println(getTableScheme(connection, "newtable"));
-        }
-    }
-    /** Удаляет столбец из таблицы */
-    public void dropColumn(String tableName, String columnName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("ALTER TABLE " + tableName + " DROP COLUMN " + columnName);
-            System.out.println(getTableScheme(connection, "newtable"));
-        }
-    }
-    /** Переименовывает столбец */
-    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("sp_rename '" + tableName + "'.'" + columnName + "', '" + newColumnName + "' 'COLUMN'");
-            System.out.println(getTableScheme(connection, "newtable"));
-        }
+        initAction("DROP TABLE " + tableName + ";");
     }
 
-    /** Вывод схемы таблицы */
+    /**
+     * Добавляет столбец в таблицу
+     */
+    public void addColumn(String tableName, String columnName, String type) throws Exception {
+        initAction("ALTER TABLE " + tableName + " ADD COLUMN " + columnName + " " + type + ";");
+    }
+
+    /**
+     * Удаляет столбец из таблицы
+     */
+    public void dropColumn(String tableName, String columnName) throws Exception {
+        initAction("ALTER TABLE " + tableName + " DROP COLUMN " + columnName + ";");
+    }
+
+    /**
+     * Переименовывает столбец
+     */
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
+        initAction("ALTER TABLE " + tableName + " RENAME COLUMN " + columnName + " TO " + newColumnName + ";");
+    }
+
+    /**
+     * Вывод схемы таблицы
+     */
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
         var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
@@ -111,9 +122,11 @@ public class TableEditor implements AutoCloseable {
     }
 
     public static void main(String[] args) throws Exception {
-        Properties properties = new Properties();
-        properties.load(new FileReader("./app.properties"));
-        TableEditor tableEditor = new TableEditor(properties);
+        Properties config = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("app.properties")) {
+            config.load(in);
+        }
+        TableEditor tableEditor = new TableEditor(config);
         tableEditor.createTable("newtable");
         tableEditor.addColumn("newtable", "first", "int");
         tableEditor.renameColumn("newtable", "first", "second");
